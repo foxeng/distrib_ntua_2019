95108cf30523e374f0295804eb5bea1d3aaae842
Michalis Xefteris <mxefteris@hotmail.com>
2019-03-14 18:41:45 +0200

    update chatter

--- chatter/chatter.py
+++ chatter/chatter.py
@@ -1,31 +1,24 @@
 import requests
+from multiprocessing.pool import ThreadPool
 
-def err_print(status_code):
-    if not (status_code == requests.codes.ok):
-        print("Some error occurred\n")
-        quit()
+def send_post_request(node_id, payload, arg):
+    r = requests.post(get_ip(node_id)["ipAddr"] + ":" + get_ip(node_id)["port"] + "/" + arg, json=payload)
 
-def broadcast_transaction(trans_data):
+def send_get_request(node_id, payload, arg):
+    r =  requests.get(get_ip(node_id)["ipAddr"] + ":" + get_ip(node_id)["port"] + "/" + arg, data=payload)
+
+def broadcast_transaction(trans_data, node_ids):
     payload = {"transData" : trans_data.dumps()}
-    ip_addresses = get_ips()
-    for ip in ip_addresses:
-        r = requests.post(ip + ":5000/transaction", json=payload)
-        err_print(r.status_code)
+    p = ThreadPool(len(node_ids))
+    results = p.map(send_post_request, node_ids, payload, "transaction")
 
-def broadcast_block(block_data):
+def broadcast_block(block_data, node_ids):
     payload = {"blockData" : block_data.dumps()}
-    ip_addresses = get_ips()
-    for ip in ip_addresses:
-        r = requests.post(ip + ":5000/block", json=payload)
-        err_print(r.status_code)
+    p = ThreadPool(len(node_ids))
+    results = p.map(send_post_request, node_ids, payload, "block")
 
-def get_blockid(blockId):
+def get_blockid(blockId, node_ids):
     payload = {"block" : blockId}
-    ip_addresses = get_ips()
-    for ip in ip_addresses:
-        r =  requests.get(ip + ":5000/block", data=payload)
-        err_print(r.status_code)
-
-#def get_ips():
-#   TODO
-#returns a list of all ip addresses except from local ip
+    p = ThreadPool(len(node_ids))
+    results = p.map(send_get_request, node_ids, payload, "block")
+   
